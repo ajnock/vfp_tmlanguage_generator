92547e7f61a1b01696950f94228cae0f6cfc24e3
Adam Nock <ajnock@ncsu.edu>
2018-09-18 12:21:46 -0400

    Adding install script for mac

---
+++ vfp_tmlanguage/install_on_mac.sh
@@ -0,0 +1,7 @@
+mkdir ~/.vscode/extensions/vfp/
+mkdir ~/.vscode/extensions/vfp/syntaxes/
+mkdir ~/.vscode/extensions/vfp/snippets/
+cp ./vfp.tmLanguage ~/.vscode/extensions/vfp/syntaxes/
+cp ./vfp.snippets.json ~/.vscode/extensions/vfp/snippets/
+cp ./package.json ~/.vscode/extensions/vfp/
+cp ./vfp.configuration.json ~/.vscode/extensions/vfp/
